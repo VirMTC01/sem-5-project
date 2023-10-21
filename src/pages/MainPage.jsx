ca383e4572f8b4566e13a3d0faa8288c1637f1fb
VIR <virajnmehta01@gmail.com>
2023-10-21 19:43:36 +0530

    5-Main Page Structure

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,15 +1,40 @@
-import React  from 'react';
+import React from 'react';
 import './mainpage.css'
 import { NavLink } from 'react-router-dom';
-const MainPage = ()=>{
+import logo from '../images/login-img.png'
+const MainPage = () => {
   return (
     <>
-     <div id='box3'>
-    <h1>Main-Page!!!!</h1>   
-    <NavLink className={'nav'} to={'/'}>Home</NavLink>
-    <NavLink className={'nav'} to={'/Dashboard'}>Dashboard</NavLink>
-    <NavLink className={'nav'} to={'/MainPage'}>Main-Page</NavLink>
-    </div> 
+    <main>
+      <div id='nav-box'>
+        {/* <h1>Main-Page!!!!</h1> */}
+        <NavLink className={'nav'} to={'/'}>Home</NavLink>
+        <NavLink className={'nav'} to={'/Dashboard'}>Dashboard</NavLink>
+        <NavLink className={'nav'} to={'/MainPage'}>Main-Page</NavLink>
+        <img src={logo} alt='logo' id='logo'/>
+      </div>
+      <div id='main-box'>
+      <div id="file-sec">
+         <h3 style={{margin:'0'}}>file-section</h3>
+      </div>
+      <div id="code-box">
+
+      <div id="editor-container">
+          <h1>editor</h1>
+      </div>
+      <div id="output-window">
+          <div id="output">
+            <h1>output</h1>
+          </div>
+          <div id="input">
+            <h1>input</h1>
+          </div>
+      </div>
+      </div>
+      </div>
+
+
+      </main>
     </>
 
   );
